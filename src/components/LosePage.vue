d7d9bee92887f9206ea4ae0df23fbf4f224d9c54
drewbeno1 <benodrew12@gmail.com>
2024-05-21 17:24:39 -0400

    update losePage

--- src/components/LosePage.vue
+++ src/components/LosePage.vue
@@ -33,7 +33,7 @@ const props = defineProps({
                   </div>
                 </div>
                 <div class="mt-5 sm:mt-6">
-                    <button type="button" class="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" @click="props.shareScore">Share Score (Coming Soon...)</button>
+                    <button type="button" class="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" @click="props.shareScore">Share Score</button>
                 </div>
               </DialogPanel>
             </TransitionChild>
