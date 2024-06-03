acff29dc6667019412a3343cd0b58e754faa5453
drewbeno1 <benodrew12@gmail.com>
2024-06-02 23:21:19 -0400

    new week

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,47 +1,47 @@
-import monday from './images/pahokee.png';
-import tuesday from './images/johnny2021.png';
-import wednesday from './images/drew2011.png';
-import thursday from './images/tye2020.png';
-import friday from './images/papacrane2011.png';
-import saturday from './images/carly2017.png';
-import sunday from './images/Hoover1937.png';
+import monday from './images/livelies2015.png';
+import tuesday from './images/wedgworth1932.jpeg';
+import wednesday from './images/woofters2012.png';
+import thursday from './images/santapapa2021.png';
+import friday from './images/carly2001.png';
+import saturday from './images/ava2013.png';
+import sunday from './images/jena2013.png';
 
 
 export default {
   images: [
     { day: 'Monday',
       src: monday,
-      year: '1941',
+      year: '2015',
     },
     {
       day: 'Tuesday',
       src: tuesday,
-      year: '2021',
+      year: '1932',
     },
     {
       day: 'Wednesday',
       src: wednesday,
-      year: '2011',
+      year: '2012',
     },
     {
       day: 'Thursday',
       src: thursday,
-      year: '2020',
+      year: '2021',
     },
     {
       day: 'Friday',
       src: friday,
-      year: '2011',
+      year: '2001',
     },
     {
       day: 'Saturday',
       src: saturday,
-      year: '2017',
+      year: '2013',
     },
     {
       day: 'Sunday',
       src: sunday,
-      year: '1937',
+      year: '2013',
     }
   ],
 };
